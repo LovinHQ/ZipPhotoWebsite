cbfb8fbe0374adc5d1325731b026693c30cf5fb3
Dott <chenyongarch@gmail.com>
2022-08-21 01:24:33 +0800

    feat: add google analytics

--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -5,6 +5,14 @@ export default function Document() {
     <Html className="h-full bg-gray-50 antialiased" lang="en">
       <Head>
         <script async src="https://cdn.splitbee.io/sb.js"></script>
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-FB3H3PD7Z7"></script>
+        <script>
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-FB3H3PD7Z7');
+        </script>
       </Head>
       <body className="flex h-full flex-col">
         <Main />
