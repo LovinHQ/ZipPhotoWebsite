4cbb034515a34a78a1dd8e30659775573306632a
Dott <chenyongarch@gmail.com>
2022-08-21 01:05:20 +0800

    feat: add splitbee analytics

--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -3,7 +3,9 @@ import { Head, Html, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html className="h-full bg-gray-50 antialiased" lang="en">
-      <Head />
+      <Head>
+        <script async src="https://cdn.splitbee.io/sb.js"></script>
+      </Head>
       <body className="flex h-full flex-col">
         <Main />
         <NextScript />
