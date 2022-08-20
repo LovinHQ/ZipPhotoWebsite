4e3dbf2ffa61d26a2ba53876760382d193cbc40f
vecpeng <841298391@qq.com>
2022-08-21 01:42:19 +0800

    fix: goole analytics

--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -1,18 +1,29 @@
 import { Head, Html, Main, NextScript } from 'next/document'
-
+import Script from 'next/script'
 export default function Document() {
   return (
     <Html className="h-full bg-gray-50 antialiased" lang="en">
       <Head>
         <script async src="https://cdn.splitbee.io/sb.js"></script>
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-FB3H3PD7Z7"></script>
-        <script>
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
+        <Script         
+          strategy="afterInteractive"
+          src="https://www.googletagmanager.com/gtag/js?id=G-FB3H3PD7Z7">
+        </Script>
+        <Script
+          id="gtag-init"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){
+                window.dataLayer.push(arguments)
+              }
+              gtag('js', new Date());
 
-          gtag('config', 'G-FB3H3PD7Z7');
-        </script>
+              gtag('config', 'G-FB3H3PD7Z7');
+            `,
+        }}
+        />
       </Head>
       <body className="flex h-full flex-col">
         <Main />
